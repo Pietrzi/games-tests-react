a4ba9bb0ab42631ee2db5944c71822a62a4d11b3
Pietrzi <pietrzi@op.pl>
2020-01-16 21:40:26 +0100

    started buildings

--- src/components/CityMap.js
+++ src/components/CityMap.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import LumberMill from './buildings/LumberMill';
+import CityHall from './buildings/CityHall';
+
 
 const CityMap = () => {
     return (
         <div>
-            
+            <CityHall />
+            <LumberMill />
         </div>
     )
 }
---
+++ src/components/buildings/LumberMill.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LumberMill = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default LumberMill;
