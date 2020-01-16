8fc39da6c3c33b6305c59de2bde14a80c705f346
Pietrzi <pietrzi@op.pl>
2020-01-16 22:55:44 +0100

    still basic construction

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CityMap from './CityMap';
 import InfoBar from './InfoBar';
 import ToolBar from './ToolBar';
+import Map from './Map';
+import Hero from './Hero';
 
 const Game = () => {
    
---
+++ src/contexts/Resources.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Resources = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Resources;
